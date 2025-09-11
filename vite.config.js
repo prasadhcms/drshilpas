9b45cea939b8be7e4c4e66e8cfda741c9d43a6ed
prasadhcms <cmswebdev.india@gmail.com>
2025-09-12 00:54:49 +0530

    supabase issue solved

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      external: ['@supabase/supabase-js'],
-    },
-  },
+  // build options can go here if needed
 })
